3c81d1d6af31eddf4ca28b5fbceffea9419ff458
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2024-07-07 13:35:57 +0000

    nice edition

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
 use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct RouteTree {
     route: RouteInTree,
-    children: Box<HashMap<String, RouteTree>>,
+    children: HashMap<String, RouteTree>,
+    dynamic_children: Option<Box<(DynamicRouteType, RouteTree)>>,
+    
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum RouteInTree {
     NoRoute,
     RouteExists(Route),
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum DynamicRouteType {
+    Single,
+    Many,
+}
+
 #[derive(Debug)]
 pub struct Routes {
-    routes: HashMap<String, RouteTree>,
-    prefix: Option<String>,
+    routes: RouteTree,
+    prefix: RoutePrefix,
+}
+
+#[derive(Debug)]
+pub enum RoutePrefix {
+    /// Root == "/"
+    Root,
+    /// This would be a path where the router gets "mounted" on to, for example /api
+    Mount(String)
 }
 
 fn main() {
@@ -24,7 +40,8 @@ fn main() {
     routes_in_auth.insert(
         "register".into(),
         RouteTree {
-            children: Box::new(HashMap::new()),
+            dynamic_children: None,
+            children: HashMap::new(),
             route: RouteInTree::RouteExists(Route {
                 method: RouteMethod::Post,
                 handler: Box::new(|| {}),
@@ -34,7 +51,8 @@ fn main() {
     routes_in_auth.insert(
         "login".into(),
         RouteTree {
-            children: Box::new(HashMap::new()),
+            children: HashMap::new(),
+            dynamic_children: None,
             route: RouteInTree::RouteExists(Route {
                 method: RouteMethod::Patch,
                 handler: Box::new(|| {}),
@@ -46,26 +64,81 @@ fn main() {
     routes.insert(
         "auth".into(),
         RouteTree {
+            dynamic_children: None,
             route: RouteInTree::NoRoute,
-            children: Box::new(routes_in_auth),
+            children: routes_in_auth,
+        },
+    );
+
+
+    routes.insert(
+        "testing".into(),
+        RouteTree {
+            dynamic_children: Some(Box::new((
+                DynamicRouteType::Single,
+                RouteTree {
+                    children: HashMap::new(),
+                    dynamic_children: None,
+                    route: RouteInTree::RouteExists(
+                        Route {
+                            method: RouteMethod::Post,
+                            handler: Box::new(|| {})
+                        }
+                    )
+                }
+            ))),
+            route: RouteInTree::RouteExists(Route {
+                method: RouteMethod::Get,
+                handler: Box::new(|| {}),
+            }),
+            children: HashMap::new(),
         },
     );
 
     let tree = Routes {
-        prefix: None,
-        routes,
+        prefix: RoutePrefix::Root,
+        routes: RouteTree {
+            route: RouteInTree::NoRoute,
+            children: routes,
+            dynamic_children: None
+        },
     };
 
     println!("{tree:#?}");
+
+    let query: Vec<&str> = "/auth/login".trim_matches('/').split('/').collect();
+
+    let mut index = 0;
+    let mut current_route = None;
+
+    let query_length = query.len();
+
+    let mut routes_to_check = tree.routes;
+
+    loop {
+        if index > query_length - 1 {
+            break;
+        }
+
+        if let Some(next_routes) = routes_to_check.children.get(query[index]).cloned() {
+            current_route = Some(next_routes.clone());
+            routes_to_check = next_routes.children.get(query[index]);
+            index += 1;
+        } else {
+            break;
+        }
+    }
+
+    println!("{:?}", &current_route);
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Route {
     method: RouteMethod,
-    handler: Box<fn()>,
+    handler: Box<fn() -> ()>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum RouteMethod {
     Get,
     Post,
