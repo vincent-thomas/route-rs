a79dd3feae84e7bd308599bf04bb56fee990d00b
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2025-01-07 02:46:03 +0100

    when will this work

---
+++ scripts/publish.sh
@@ -0,0 +1,9 @@
+cargo publish --manifest-path titan-utils/Cargo.toml
+cargo publish --manifest-path titan-html-derive/Cargo.toml
+cargo publish --manifest-path titan-html/Cargo.toml
+
+cargo publish --manifest-path titan-router/Cargo.toml
+cargo publish --manifest-path titan-http/Cargo.toml
+cargo publish --manifest-path titan-core/Cargo.toml
+cargo publish --manifest-path titan-server/Cargo.toml
+cargo publish --manifest-path titan/Cargo.toml
--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-
-sed -i "s/version = \"$1\"/version = \"$2\"/g" Cargo.toml titan-*/Cargo.toml
+sed -i "s/version = \"$1\"/version = \"$2\"/g" Cargo.toml titan-*/Cargo.toml titan/Cargo.toml
